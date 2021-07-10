16d6066e6ffdc0e3b95e6e52e8a0eef7fdb0cb1c
Milton Kabir <drifter.kabir@gmail.com>
2021-07-10 17:01:05 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,18 @@
 package com.kabir.milton;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc=new Scanner(System.in);
+        String st=sc.nextLine();
+        if(st.length()>100){
+            System.out.println("HARD");
+        }
+        else{
+            System.out.println("EASY");
+        }
     }
 }
